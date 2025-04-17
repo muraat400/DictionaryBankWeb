36a3bae453e046cf2608996fdb0b762d3ca7872f
murat <murat@MURAT>
2025-04-17 18:55:34 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WordService, WordsData, Word } from './word.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  providers: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  wordsData!: WordsData;
   title = 'DictionaryBankWeb';
+
+  constructor(private wordService: WordService) { }
+
+  ngOnInit() {
+    this.wordService.getWords().subscribe(data => {
+      this.wordsData = data;
+    });
+  }
 }
---
+++ src/app/word.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Sentence {
+  SentenceEn: string;
+  SentenceTr: string;
+}
+
+export interface Word {
+  Id: number;
+  English: string;
+  Turkish: string[];
+  Sentence: Sentence[];
+  Pronunciation: string;
+  Notes: string[];
+  CreatedOn: string;
+  UpdatedOn: string;
+}
+
+export interface WordsData {
+  Words: Word[];
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WordService {
+  private jsonUrl = 'assets/output.json';
+
+  constructor(private http: HttpClient) { }
+
+  getWords(): Observable<WordsData> {
+    return this.http.get<WordsData>(this.jsonUrl);
+  }
+}
